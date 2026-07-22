e1102f376c624c2568d6fcf7e96b59705e5e1dc1
Hansraj Meena <hansrajmeena@Hansrajs-MacBook-Air.local>
2026-07-22 15:08:49 +0530

    Revert production database path

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-const dbPath =
-    process.env.RENDER
-        ? "/var/data/align.db"
-        : path.join(__dirname, "database", "align.db");
-
+const dbPath = path.join(__dirname, "database", "align.db");
 console.log("Using DB:", dbPath);
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
