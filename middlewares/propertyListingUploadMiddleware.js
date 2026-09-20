dc7db080f45083022be825afc937c28139f803fb
Hansraj Meena <hans004333@gmail.com>
2026-09-21 01:12:41 +0530

    Fix Render upload path

--- middlewares/propertyListingUploadMiddleware.js
+++ middlewares/propertyListingUploadMiddleware.js
@@ -10,7 +10,7 @@ const fs =
 
 const uploadsPath =
     process.env.RENDER
-        ? "/var/data/uploads"
+        ? "/tmp/uploads"
         : path.join(
             __dirname,
             "../uploads"
